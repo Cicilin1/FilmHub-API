c9d196c95f40fa35523d6b26d141db8687b2c60e
Cicilini <guilherme.cicilini@hotmail.com>
2024-04-29 21:10:32 -0300

    database connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import { connectDatabase } from './database/db.js';
 
 const app = express();
 const port = 3004
 app.use(express.json())
 
-app.get('/', (req,res) => res.json("Hello world"))
+app.get('/', (req, res) => res.json("Hello world"))
 
+connectDatabase().then(() => {
+    app.listen(port, () => console.log("🚀 server and DataBase running in port:", port))
 
-app.listen(port, () => console.log("🚀 server running in port:", port))
+}).catch((e) => console.log(e))
+
